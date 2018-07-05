04a2c9924d6d9d3ba62d656680d41eec132bbaa6
Marc Catrisse <marc@catrisse.net>
2018-07-05 11:28:49 +0200

    notification

--- app/src/main/java/catrisse/marc/calculadora/LoginActivity.java
+++ app/src/main/java/catrisse/marc/calculadora/LoginActivity.java
@@ -1,8 +1,13 @@
 package catrisse.marc.calculadora;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -42,6 +47,7 @@ public class LoginActivity extends AppCompatActivity {
                     startActivity(i);
                 }else{
                     Toast.makeText(getApplicationContext(),"Login incorrecto", Toast.LENGTH_LONG).show();
+                    mostrar_notificacion(aux.getNom());
                 }
             }
         });
@@ -53,6 +59,41 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private void mostrar_notificacion(String nom) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext(),"default") //se que esta deprecated, a partir de la 26 te obligan a usar un NotificationChannelID
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentText(String.format("Se ha intentado hacer login con el usuario %s sin éxito",nom))
+                .setContentTitle("Notificación");
+        Intent resultIntent = new Intent(getApplicationContext(), LoginActivity.class);
+        // Indicamos que la Activity empiece en una nueva task
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        // Creamos el PendingIntent
+        PendingIntent notifyPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(notifyPendingIntent);
+
+        NotificationManager
+                mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        int myId = 2;
+        Notification notification = mBuilder.build();
+        notification.flags |= Notification.FLAG_NO_CLEAR; //añadimos flag para que no se pueda 'limpiar'
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel  nc = new NotificationChannel("default",getString(R.string.channelName)
+                    ,NotificationManager.IMPORTANCE_LOW);
+            mNotificationManager.createNotificationChannel(nc);
+        }
+        mNotificationManager.notify(myId,notification);
     }
 
 }
