34d7cb8f104d73bc16b6557ceed8b9bfe013b9af
Marc Catrisse <marc@catrisse.net>
2018-07-11 22:44:10 +0200

    timer working

--- app/src/main/java/catrisse/marc/calculadora/GameFragment.java
+++ app/src/main/java/catrisse/marc/calculadora/GameFragment.java
@@ -1,6 +1,7 @@
 package catrisse.marc.calculadora;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,13 +10,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GameFragment extends Fragment {
-
+    View layoutGame;
+    TimerTask timerTask;
+    TextView time;
 
     public GameFragment() {
         // Required empty public constructor
@@ -27,15 +31,26 @@ public class GameFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_game, container, false);
+        layoutGame = inflater.inflate(R.layout.fragment_game, container, false);
+        time = layoutGame.findViewById(R.id.drawerTextTime);
+        timerTask = null;
+        return layoutGame;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.game_start:
+                if(timerTask == null ||(timerTask.getStatus() != AsyncTask.Status.RUNNING)){
+                    timerTask = new TimerTask();
+                    timerTask.execute();
+                }
                 return true;
             case R.id.game_stop:
+                if(timerTask != null && timerTask.getStatus() == AsyncTask.Status.RUNNING) {
+                    timerTask.cancel(true);
+                    timerTask = null; //limpiamos timer task
+                }
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -46,5 +61,32 @@ public class GameFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.activity_drawer_game, menu);
     }
+
+    private class TimerTask extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Integer progress = 0;
+            while(!isCancelled()){
+                try {
+                    Thread.sleep(1000);
+                    progress += 1000;
+                    publishProgress(progress);
+                } catch (InterruptedException e) {
+                    e.printStackTrace(); //no se deberia interrumpir nunca...
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            update_timer(values[0]/1000);
+        }
+    }
+
+    private void update_timer(Integer val) {
+        time.setText(val.toString());
+    }
+
 
 }
