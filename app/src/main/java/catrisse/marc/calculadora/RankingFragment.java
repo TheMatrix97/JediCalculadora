5ac139a072b5fa923f0226ad80cb0fca922aac28
Marc Catrisse <marc@catrisse.net>
2018-07-13 12:26:58 +0200

    starting with ranking... pager

--- app/src/main/java/catrisse/marc/calculadora/RankingFragment.java
+++ app/src/main/java/catrisse/marc/calculadora/RankingFragment.java
@@ -1,14 +1,19 @@
 package catrisse.marc.calculadora;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -31,14 +36,39 @@ public class RankingFragment extends Fragment {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_ranking, container, false);
         init();
+        configurar_pager();
 
         return rootView;
+    }
+
+    private void configurar_pager() {
+
     }
 
     private void init() {
         pager = rootView.findViewById(R.id.pager);
         actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
         actionBar.setTitle(R.string.ranking);
+    }
+
+    private class PagerAdapter extends FragmentPagerAdapter{
+        ArrayList<Fragment> collection;
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 
 }
