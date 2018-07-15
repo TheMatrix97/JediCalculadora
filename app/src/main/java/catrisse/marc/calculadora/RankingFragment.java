652dd0630468e0f90385adfcccfc4d74a72b1a55
Marc Catrisse <marc@catrisse.net>
2018-07-15 17:21:44 +0200

    Add tabs

--- app/src/main/java/catrisse/marc/calculadora/RankingFragment.java
+++ app/src/main/java/catrisse/marc/calculadora/RankingFragment.java
@@ -2,6 +2,7 @@ package catrisse.marc.calculadora;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -22,6 +23,7 @@ public class RankingFragment extends Fragment {
     View rootView;
     ViewPager pager;
     ActionBar actionBar;
+    TabLayout tabLayout;
 
 
     public RankingFragment() {
@@ -42,8 +44,27 @@ public class RankingFragment extends Fragment {
 
     private void configurar_pager() {
         PagerAdapter adapter = new PagerAdapter(getChildFragmentManager());
-        ViewPager vp = rootView.findViewById(R.id.pager);
-        vp.setAdapter(adapter);
+        pager = rootView.findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+        tabLayout = rootView.findViewById(R.id.tabLayout);
+        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
 
     }
 
